0cd5e49292b36cdddccdaa2674307a0b42238c13
Евгений Емелин [FE] <emelin.eug@eggheads.solutions>
2024-02-26 19:05:18 +0300

    Перенес логику на фронт

---
+++ src/constants/resources.const.ts
@@ -0,0 +1,12 @@
+export const BET_DEPOSIT_CONTRACT_URL =
+  'https://etherscan.io/address/0x24c5a43a0d5afd8d4a1afd9d262af6b0d43a5fa9#writeContract';
+
+export const ADDICTION_BET_NODE =
+  'wss://eth-mainnet.g.alchemy.com/v2/ScK1WWZHRPgt8xtmGzmaPlCGLjX7VQPp';
+
+export const DEFAULT_SIGNER_KEY =
+  '0x0123456789012345678901234567890123456789012345678901234567890123';
+
+// export const DEFAULT_PROVIDER = 'https://rpc.sepolia.org';
+export const DEFAULT_PROVIDER =
+  'wss://eth-mainnet.g.alchemy.com/v2/hOIqxhnlrC5oEsJIK7IthLlfPx6BdguH';
